0b6b1b097c950bcff29eb5bd9f8bdc06dc5b8996
tejas100 <tejas55bk@gmail.com>
2023-11-26 20:05:01 -0500

    FinishedImageUplaoad

--- plantDisease/settings.py
+++ plantDisease/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,9 +29,14 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+# Media URL
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
+
 # Application definition
 
 INSTALLED_APPS = [
+    'proc.apps.ProcConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -116,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- proc/views.py
+++ proc/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse, request
+from django.views.generic import TemplateView
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
 
 def index(request):
-    return render(request, "index.html")
+     if request.method == 'POST':
+        uploaded_file = request.FILES['document']
+        fs = FileSystemStorage()
+        name = fs.save(uploaded_file.name, uploaded_file)
+        print(name)
+        # print(uploaded_file.size)
+     return render(request, "index.html")
+
